b18f2aa0a9f98938b8215d4860ac4c54bca01f75
sonu <okcover80@gmail.com>
2020-10-07 23:17:18 +0700

    materi pertama

---
+++ application/controllers/Kalkulator.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kalkulator extends CI_Controller {
+
+    public function index(){
+        $panjang = 5;
+       $Panjang = 10-7;
+        echo "panjang: " . $panjang;
+    }
+
+}
---
+++ application/controllers/User.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+    public function index(){
+        echo "Selamat Datang";
+    }
+
+    public function tambah(){
+        echo "fungsi tambah";
+    }
+    
+}
